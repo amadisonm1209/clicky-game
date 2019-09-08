71918da07c41c6aad94112f8a0b7e5b6de6023bb
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-09-08 17:55:00 -0600

    added css

--- clicky-game/src/components/Scoreboard/Scoreboard.js
+++ clicky-game/src/components/Scoreboard/Scoreboard.js
@@ -6,15 +6,15 @@ const Scoreboard = (props) => {
     <nav className="scoreboard container">
       <div className="row greeting">
         <div className="">
-          <h2>Wes Anderson Click-Game</h2>
-          <h4>Click each picture only one time, or suffer through more of my rhyme. <br /> If you click on one you've clicked before, you'll click no more.</h4>
+          <h2>Wes Anderson Click Game</h2>
+          <h4>Click each picture only one time and try to remember what you've clicked!</h4>
         </div>
       </div>
       <div className="instructions">
         <h4>{props.feedback}</h4>
       </div>
       <div className="scores">
-        <p>Your Score: {props.score} ----- High Score: {props.highScore}</p>
+        <p>Your Score: {props.score}   |   High Score: {props.highScore}</p>
       </div>
     </nav>
   )
