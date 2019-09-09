ccd4e92a24fe4bb5a03ec468d7568e29d134c768
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-09-08 18:09:08 -0600

    Updates

---
+++ precache-manifest.c8edc5583c5df18c7f0449656d7be887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1124c3fb00a5efb032bd65f2403781f4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c7ef1d530f429aeb310c",
+    "url": "/clicky-game/static/css/main.1d18ebcc.chunk.css"
+  },
+  {
+    "revision": "c02afe3181c5d93d8299",
+    "url": "/clicky-game/static/js/2.86a6994e.chunk.js"
+  },
+  {
+    "revision": "c7ef1d530f429aeb310c",
+    "url": "/clicky-game/static/js/main.11ff4373.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
